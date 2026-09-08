87a8554e502a1f3f6a1a0220c4450174027017a6
asmitdahal651-sketch <asmitdahal651@gmail.com>
2026-09-08 08:12:13 +0545

    new

---
+++ week-2/script.js
@@ -0,0 +1,6 @@
+ function add()
+        {
+            let num1 = Number (document.getElementById("num1").value);
+            let num2 = Number (document.getElementById("num2").value);
+            document.getElementById("number").innerHTML = num1 + num2;
+        }
