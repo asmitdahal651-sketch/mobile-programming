2f9ea9d422374916a17d44c374419739a8b11b45
asmitdahal651-sketch <asmitdahal651@gmail.com>
2026-09-08 08:17:43 +0545

    Added all the arthemetic experession

--- week-2/script.js
+++ week-2/script.js
@@ -4,3 +4,18 @@
             let num2 = Number (document.getElementById("num2").value);
             document.getElementById("number").innerHTML = num1 + num2;
         }
+function subtract()
+        {
+            let num1 = Number (document.getElementById("num1").value);
+            let num2 = Number (document.getElementById("num2").value);
+            document.getElementById("number").innerHTML = num1 - num2;
+        }
+function multiply()
+        {
+            let num1 = Number (document.getElementById("num1").value);
+            let num2 = Number (document.getElementById("num2").value);
+            document.getElementById("number").innerHTML = num1 * num2;
+        }
+
+        
+        
