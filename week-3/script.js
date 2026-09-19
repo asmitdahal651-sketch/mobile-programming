fa7a50c855fafd4b327287777bd209aa3b204201
asmitdahal651-sketch <asmitdahal651@gmail.com>
2026-09-19 21:52:17 +0545

    updated

--- week-3/script.js
+++ week-3/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
+
   requestAnimationFrame(function () {
     $("#card").addClass("is-in");
   });
 
   $("#qr").on("click", function () {
-    $("#show").addClass("is-open").slideDown(260);
+    $("#show").addClass("is-open").fadeIn(200);
   });
 
   $("#hide").on("click", function () {
-    $("#show").slideUp(200, function () {
+    $("#show").fadeOut(150, function () {
       $(this).removeClass("is-open");
     });
   });
