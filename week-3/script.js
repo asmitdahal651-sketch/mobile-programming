c3a77e50b6a60341faf75857d66340140f2fdc47
asmitdahal651-sketch <asmitdahal651@gmail.com>
2026-09-19 22:51:01 +0545

    updated

--- week-3/script.js
+++ week-3/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
-
+  // one orchestrated entrance: card rises, sprig blooms
   requestAnimationFrame(function () {
     $("#card").addClass("is-in");
+  });
+
+  $("#card").on("mouseenter", function () {
+    $(this).css({
+      "background-color": "#1e3a2c",
+      color: "#f4efe1",
+    });
+  });
+
+  $("#card").on("mouseleave", function () {
+    $(this).css({
+      "background-color": "#f4efe1",
+      color: "#3a2a1c",
+    });
   });
 
   $("#qr").on("click", function () {
